fe713df3fba2c49457674e6d6b8cd695a096a5f2
Yasmeen Mohamed <135316122+YasmeenMohamed1@users.noreply.github.com>
2024-09-21 00:27:31 +0300

    Add files via upload

---
+++ src/Components/messageAdd.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { FaCheckCircle, FaTimes } from 'react-icons/fa';
+
+
+const MessageAdd = ({onClose}) => {
+  
+  return (
+    <div
+              style={{
+                position: 'fixed',
+                top: '50%',
+                left: '50%',
+                transform: 'translate(-50%, -50%)',
+                backgroundColor: 'white',
+                padding: '30px',
+                borderRadius: '10px',
+                boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)',
+                zIndex: 1000,
+                textAlign: 'center',
+              }}
+            >
+              
+                <FaTimes
+                  size={20}
+                  style={{
+                    position: 'absolute',
+                    top: '10px',
+                    right: '10px',
+                    cursor: 'pointer',
+                    color: '#333',
+                  }}
+                  onClick={onClose}
+                />
+    
+                <FaCheckCircle size={50} color="#ECF2FF" />
+                
+                <h4 className="my-3">إضافة الشحنة كملف  اكسيل</h4>
+                
+                <button
+                 onClick={onClose}
+                  style={{
+                    padding: '10px 20px',
+                    backgroundImage: 'linear-gradient(180deg, #135D66 0%, #22A6B6 100%)',
+                    color: '#fff',
+                    border: 'none',
+                    borderRadius: '5px',
+                    cursor: 'pointer',
+                  }}
+                >
+    
+                اضافه
+                </button>
+              </div>
+   
+  );
+};
+
+export default MessageAdd;
