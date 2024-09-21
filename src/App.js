2f931105268e2a69dd32c3567334690b3c08144a
Yasmeen Mohamed <135316122+YasmeenMohamed1@users.noreply.github.com>
2024-09-21 20:13:08 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from './Pages/login';
 import AddUser from './Pages/addUser';
 import Profile from './Pages/profile';
+import Dashboard from './Pages/dashboard';
 
 function App() {
   return (
     <div dir="rtl">
       <Router>
-        <div className=" p-33px" style={{ flex: 1 }}>
+        <div className=" p-33px">
           <Routes>
             <Route path="/login" element={<Login />} exact />
             <Route path="/addUser" element={<AddUser />} exact />
             <Route path="/profile" element={<Profile />} exact />
+            <Route path="/dashboard" element={<Dashboard />} exact />
           </Routes>
         </div>
       </Router>
