630e66949cb40dcc845702f07963fd3eac100323
Yasmeen Mohamed <135316122+YasmeenMohamed1@users.noreply.github.com>
2024-09-21 22:36:18 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Login from './Pages/login';
 import AddUser from './Pages/addUser';
 import Profile from './Pages/profile';
 import Dashboard from './Pages/dashboard';
+import Finance from './Pages/finance';
+import Users from './Pages/users';
+import DeliveryShipment from './Pages/deliveryShipments';
 
 function App() {
   return (
@@ -12,10 +15,13 @@ function App() {
       <Router>
         <div className=" p-33px">
           <Routes>
-            <Route path="/login" element={<Login />} exact />
-            <Route path="/addUser" element={<AddUser />} exact />
-            <Route path="/profile" element={<Profile />} exact />
-            <Route path="/dashboard" element={<Dashboard />} exact />
+            <Route path="/login" element={<Login/>} exact />
+            <Route path="/addUser" element={<AddUser/>} exact />
+            <Route path="/profile" element={<Profile/>} exact />
+            <Route path="/dashboard" element={<Dashboard/>} exact />
+            <Route path="/finance" element={<Finance/>} exact />
+            <Route path="/users" element={<Users/>} exact />
+            <Route path="/deliveryShipment" element={<DeliveryShipment/>} exact />
           </Routes>
         </div>
       </Router>
